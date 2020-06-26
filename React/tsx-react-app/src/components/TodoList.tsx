6d6d205a7613cd819f38406b221a8bea69facf98
NoorShinwari <nanoshin746@gmail.com>
2020-06-26 11:14:32 +0200

    Working with Props and Types for Props

---
+++ React/tsx-react-app/src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+
+interface TodoListProps {
+   items: { id: string;
+    text: string}[];
+}
+
+
+const TodoList: React.FC<TodoListProps> = (props) => {
+  return <ul>
+      {props.items.map((todo)=>(
+          <li key={todo.id}>{todo.text}</li>
+      ))}
+  </ul>;
+};
+
+export default TodoList;
